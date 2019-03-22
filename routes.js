45f1d34fefa117fe76f8d80955cf26026b778cec
Giacomo Benati <giacomobenati@mailbox.org>
2019-03-22 09:43:17 +0100

    added basic error handler

--- routes.js
+++ routes.js
@@ -1,9 +1,25 @@
 const express = require('express');
-const project = require('./data/helpers/projectModel');
-const action = require('./data/helpers/actionModel');
+const projects = require('./data/helpers/projectModel');
+const actions = require('./data/helpers/actionModel');
+const errors = require('./Middlewares/errors');
 
 const routes = express.Router();
 
 routes.use(express.json());
+
+//PROJECT REQUESTS
+
+routes.get('/api/projects', (req, res, next) => {
+    projects.get()
+        .then(projects => {
+            res.json(projects)
+        })
+        .catch(err => next({
+            status: 500,
+            message: "Sorry, could not get projects!"
+        }))
+})
+
+routes.use(errors.error);
 
 module.exports = routes;
